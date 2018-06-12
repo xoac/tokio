e601ac195f5c2c65118691945779d82ab68553b0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-12 21:55:47 +0000

    Deploy tokio-rs/tokio to github.com/tokio-rs/tokio.git:gh-pages

---
+++ tokio/fs/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["file","Types for working with [`File`]."]],"struct":[["File","A reference to an open file on the filesystem."],["OpenOptions","Options and flags which can be used to configure how a file is opened."]]});
